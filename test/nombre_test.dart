49267bab5203c6860c14bdb30073acf0411f8c1a
Alejandra Higuera <al4433205@gmail.com>
2025-09-17 19:37:29 -0700

    test de nombre en clases

--- test/nombre_test.dart
+++ test/nombre_test.dart
@@ -2,11 +2,28 @@ import 'package:juegonaval/nombre.dart';
 import 'package:test/test.dart';
 void main() {
   test('Debe de tener un minimo de longitud', () {
-  try {
-    Nombre nombre= Nombre('');
-    }catch (e){ 
-      expect(true, equals(true));
-      }
-      expect(true, equals(false));
+    expect(()=> Nombre(''), throwsA(isA<LongitudMinimaExcepCion>()));
+  
+  });
+ test('No debe de pasar un maximo de longitud', () {
+    expect(
+      ()=> Nombre('a12345678912345ssss6'), 
+      throwsA(isA<LongitudMaximaExcepCion>()),
+      );
+  
+  });
+   test('Se permite los caracteres normales', () {
+    Nombre nombre= Nombre('alehi');
+    expect(true, equals(true));
+  });
+  test('No se permite los caracteres extraños', () {
+     expect(()=> Nombre('aleh.'), throwsA(isA<CaracterNoPermitidoExcepcion>()));
+  });
+    test('No puede empezar con numero', () {  
+     expect(()=> Nombre('1aleh'), throwsA(isA<CaracterNoPermitidoExcepcion>()));
+  });
+     test('Se permite ñ', () {
+    Nombre nombre= Nombre('ñoñoo');
+    expect(true, equals(true));
   });
 }
