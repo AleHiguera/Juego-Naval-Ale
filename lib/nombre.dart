5bba1458b761e5bf2117d36f53978f06311b637c
Alejandra Higuera <al4433205@gmail.com>
2025-09-17 19:37:59 -0700

    Longitud minima permitida

--- lib/nombre.dart
+++ lib/nombre.dart
@@ -1,4 +1,12 @@
 class Nombre {
   late String candidato;
-Nombre(this.candidato);
+Nombre(String c){
+  if (c.length<LongitudMinimaPermitida){ 
+    throw LongitudMinimaExcepCion();
+  }
+  }
 }
+const LongitudMinimaPermitida = 5;
+
+class LongitudMinimaExcepCion extends Error{
+  }
