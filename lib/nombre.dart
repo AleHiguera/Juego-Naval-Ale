5820b327798db2df3b45476907e9d365e34cef5d
Alejandra Higuera <al4433205@gmail.com>
2025-09-17 19:53:51 -0700

    agregue la  solucion para los test

--- lib/nombre.dart
+++ lib/nombre.dart
@@ -4,9 +4,25 @@ Nombre(String c){
   if (c.length<LongitudMinimaPermitida){ 
     throw LongitudMinimaExcepCion();
   }
+      if (c.length > LongitudMaximaPermitida) {
+      throw LongitudMaximaExcepCion();
+    }
+  if (c.isNotEmpty && _isDigit(c[0])) {
+      throw CaracterNoPermitidoExcepcion();
+    }
+     final RegExp patronPermitido = RegExp(r'^[a-zA-Z0-9ñÑ]+$');
+
+    if (!patronPermitido.hasMatch(c)) {
+      throw CaracterNoPermitidoExcepcion();
+    }
+        this.candidato = c;
   }
 }
+bool _isDigit(String s) {
+  return RegExp(r'\d').hasMatch(s);
+}
 const LongitudMinimaPermitida = 5;
-
-class LongitudMinimaExcepCion extends Error{
-  }
+const LongitudMaximaPermitida = 15;
+class LongitudMinimaExcepCion extends Error{}
+class LongitudMaximaExcepCion extends Error {}
+class CaracterNoPermitidoExcepcion extends Error {}
