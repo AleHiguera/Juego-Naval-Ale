f4362c473972f5c47c0b94db7bbd1a38a4ba50b6
Alejandra Higuera <al4433205@gmail.com>
2025-09-23 13:54:56 -0700

    ajustes en clases

--- lib/nombre.dart
+++ lib/nombre.dart
@@ -1,28 +1,50 @@
+const longitudMinimaPermitida = 5;
+const longitudMaximaPermitida = 15;
+
+// Definición de las clases de excepciones para cada tipo de error
+class LongitudMinimaExcepcion extends Error {}
+class LongitudMaximaExcepcion extends Error {}
+class CaracterNoPermitidoExcepcion extends Error {}
+
 class Nombre {
   late String candidato;
-Nombre(String c){
-  if (c.length<LongitudMinimaPermitida){ 
-    throw LongitudMinimaExcepCion();
+
+  Nombre(String c) {
+    _validarLongitud(c);
+    _validarInicio(c);
+    _validarCaracteres(c);
+    
+    // Eliminamos 'this.' ya que es innecesario
+    candidato = c;
   }
-      if (c.length > LongitudMaximaPermitida) {
-      throw LongitudMaximaExcepCion();
-    }
-  if (c.isNotEmpty && _isDigit(c[0])) {
+
+  // Valida que el nombre no empiece con un numero
+  static void _validarInicio(String c) {
+    if (c.isNotEmpty && _isDigit(c[0])) {
       throw CaracterNoPermitidoExcepcion();
     }
-     final RegExp patronPermitido = RegExp(r'^[a-zA-Z0-9ñÑ]+$');
+  }
 
+  // Valida que el nombre contenga solo caracteres permitidos
+  static void _validarCaracteres(String c) {
+    final RegExp patronPermitido = RegExp(r'^[a-zA-Z0-9ñÑ]+$');
     if (!patronPermitido.hasMatch(c)) {
       throw CaracterNoPermitidoExcepcion();
     }
-        this.candidato = c;
+  }
+  
+  // Valida que el nombre cumpla con los límites de longitud
+  static void _validarLongitud(String c) {
+    if (c.length < longitudMinimaPermitida) {
+      throw LongitudMinimaExcepcion();
+    }
+    if (c.length > longitudMaximaPermitida) {
+      throw LongitudMaximaExcepcion();
+    }
   }
 }
+
+// Función auxiliar para verificar si un caracter es un digito
 bool _isDigit(String s) {
   return RegExp(r'\d').hasMatch(s);
 }
-const LongitudMinimaPermitida = 5;
-const LongitudMaximaPermitida = 15;
-class LongitudMinimaExcepCion extends Error{}
-class LongitudMaximaExcepCion extends Error {}
-class CaracterNoPermitidoExcepcion extends Error {}
--- test/nombre_test.dart
+++ test/nombre_test.dart
@@ -2,18 +2,18 @@ import 'package:juegonaval/nombre.dart';
 import 'package:test/test.dart';
 void main() {
   test('Debe de tener un minimo de longitud', () {
-    expect(()=> Nombre(''), throwsA(isA<LongitudMinimaExcepCion>()));
+expect(() => Nombre(''), throwsA(isA<LongitudMinimaExcepcion>()));
   
   });
  test('No debe de pasar un maximo de longitud', () {
     expect(
       ()=> Nombre('a12345678912345ssss6'), 
-      throwsA(isA<LongitudMaximaExcepCion>()),
+      throwsA(isA<LongitudMaximaExcepcion>()),
       );
   
   });
    test('Se permite los caracteres normales', () {
-    Nombre nombre= Nombre('alehi');
+    Nombre nombre = Nombre('alehi');
     expect(true, equals(true));
   });
   test('No se permite los caracteres extraños', () {
@@ -23,7 +23,7 @@ void main() {
      expect(()=> Nombre('1aleh'), throwsA(isA<CaracterNoPermitidoExcepcion>()));
   });
      test('Se permite ñ', () {
-    Nombre nombre= Nombre('ñoñoo');
+    Nombre nombre = Nombre('ñoñoo');
     expect(true, equals(true));
   });
 }
