43e5271a41a616e96b0ca9b6b9f1fe3ce3ac4397
Alejandra Higuera <al4433205@gmail.com>
2025-09-24 21:14:05 -0700

    ca,bie ,i codigo de nombre

--- lib/nombre.dart
+++ lib/nombre.dart
@@ -1,7 +1,7 @@
+
 const longitudMinimaPermitida = 5;
 const longitudMaximaPermitida = 15;
 
-// Definición de las clases de excepciones para cada tipo de error
 class LongitudMinimaExcepcion extends Error {}
 class LongitudMaximaExcepcion extends Error {}
 class CaracterNoPermitidoExcepcion extends Error {}
@@ -13,19 +13,16 @@ class Nombre {
     _validarLongitud(c);
     _validarInicio(c);
     _validarCaracteres(c);
-    
-    // Eliminamos 'this.' ya que es innecesario
+
     candidato = c;
   }
 
-  // Valida que el nombre no empiece con un numero
   static void _validarInicio(String c) {
     if (c.isNotEmpty && _isDigit(c[0])) {
       throw CaracterNoPermitidoExcepcion();
     }
   }
 
-  // Valida que el nombre contenga solo caracteres permitidos
   static void _validarCaracteres(String c) {
     final RegExp patronPermitido = RegExp(r'^[a-zA-Z0-9ñÑ]+$');
     if (!patronPermitido.hasMatch(c)) {
@@ -33,7 +30,6 @@ class Nombre {
     }
   }
   
-  // Valida que el nombre cumpla con los límites de longitud
   static void _validarLongitud(String c) {
     if (c.length < longitudMinimaPermitida) {
       throw LongitudMinimaExcepcion();
@@ -44,7 +40,6 @@ class Nombre {
   }
 }
 
-// Función auxiliar para verificar si un caracter es un digito
 bool _isDigit(String s) {
   return RegExp(r'\d').hasMatch(s);
 }
