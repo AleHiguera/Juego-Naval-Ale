1f6da419001b6b709a9b163431b1db4e8bf8d34a
Alejandra Higuera <al4433205@gmail.com>
2025-09-24 21:14:33 -0700

    agregue y diseñe codigo flotilla,dart

--- lib/flotilla.dart
+++ lib/flotilla.dart
@@ -1,32 +1,49 @@
-enum TiposBarcos{bote, lancha,submarino, crucero, portaaviones}
-class Barco{
-  TiposBarcos tipo;
-  Barco (this.tipo);
-}
+import 'dart:ffi';
 
+import 'package:juegonaval/elemento.dart';
+import 'package:juegonaval/punto.dart';
+enum TiposBarcos{bote, lancha,submarino, crucero, portaaviones}
+
+enum DireccionHacia{arrriba, abajo, izquierda, derecha}
+
+class Barco{
+  final TiposBarcos tipo;
+  final DireccionHacia dureccion;
+  final Punto puntoinicial;
+ List<Elemento> _elementos = [];
+  Barco ({
+    required this.tipo, 
+    required this.puntoinicial, 
+    required this.dureccion
+    }) {
+   
+  }
+}
 
 class Flotilla{
-  List<Barco> _barcos;
+  final List<Barco> _barcos;
   int get cantidad =>_barcos.length;
   Flotilla(this._barcos) {
-  if ( !esCantidadCorrecta(_barcos)) throw FlotillaCantidadExcepcio();
-  if ( !sonTiposCorrectos(_barcos)) throw FlotillaTipos();
-    if ( !estanEnPosicionAdecuada (_barcos)) throw FlotillaTipos;
+  if ( !esCantidadCorrecta(_barcos)) throw FlotillaCantidadExcepcion();
+  if ( !sonTiposCorrectos(_barcos)) throw FlotillaTiposExcepcion();
+    if ( !estanEnPosicionAdecuada (_barcos)) throw FlotillaPosicionExcepcion();
  }
+
 bool estanEnPosicionAdecuada(List<Barco> barcos){
   return false;
-}
-  
+} 
 }
 
 bool esCantidadCorrecta(List<Barco> barcos){
   return barcos.length ==5;
   }
+
   bool sonTiposCorrectos(List<Barco> barcos){
   return barcos.map ((e)=> e.tipo).toSet ().length==5;
   }
 
- 
 class FlotillaPosicionExcepcion extends Error{ } 
-class FlotillaCantidadExcepcio extends Error{ }
-class FlotillaTipos extends Error{ }
+
+class FlotillaCantidadExcepcion extends Error{ }
+
+class FlotillaTiposExcepcion extends Error{ }
