c014a4a27b102d61f2f463218f47e05436163cec
Alejandra Higuera <al4433205@gmail.com>
2025-09-30 13:14:26 -0700

    elije un turno y variable quien va

--- lib/partida.dart
+++ lib/partida.dart
@@ -1,7 +1,34 @@
-enum Estado {creada,iniciada,terminada}
-class Partida{
-   late Estado estado;
-  Partida(){
-estado= Estado.creada;
+import 'dart:math'; // Necesitas esta librería para usar Random
+
+enum Estado {
+  creada,
+  iniciada,
+  terminada
 }
+
+// Nuevo enum para representar a los dos jugadores (para los turnos)
+enum Jugador {
+  jugador1,
+  jugador2
+}
+
+class Partida {
+  late Estado estado; 
+  late Jugador quienVa; 
+  final Random _random = Random();
+
+  Partida() {
+    estado = Estado.creada; 
+    _elegirPrimerTurno();
+  }
+
+  void _elegirPrimerTurno() {
+    final int randomValue = _random.nextInt(2); 
+
+    if (randomValue == 0) {
+      quienVa = Jugador.jugador1;
+    } else {
+      quienVa = Jugador.jugador2;
+    }
+  }
 }
